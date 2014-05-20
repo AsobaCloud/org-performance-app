b65acfa9bec9d240554eae299c4f8e1f8ae26d8b
Joe Wagner <njwjs722@gmail.com>
2014-05-20 13:37:15 -0600

    Add Stylus CSS preprocessor

--- app.js
+++ app.js
@@ -8,20 +8,17 @@ var util = require('util');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var morgan = require('morgan');
-var RedisStore = require('connect-redis')(session);
 var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
+var stylus = require('stylus');
 
 
 var app = express();
 
 app.disable('x-powered-by');
 
-// define a custom log format that includes the connect defaults, and the response time
-morgan.format('response-time', ':remote-addr - - [:date] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" :response-time ms');
-
 // attach middleware
-app.use(morgan('response-time'));
-
+app.use(morgan());
 app.use(bodyParser());
 app.use(cookieParser()); // required before session.
 app.use(session({
@@ -31,6 +28,7 @@ app.use(session({
 // use passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(stylus.middleware({ src: __dirname + '/public' }));
 app.use(express.static(__dirname + '/public'));
 
 util.log('web server listening on port ' + config.port);
